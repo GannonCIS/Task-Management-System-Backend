5368da70723b6e3072d72806de977cbcdedbb057
rose030@gannon.edu <rose030@gannon.edu>
2023-04-04 13:52:26 -0400

    project model

---
+++ src/models/Project.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "please enter project name"],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: false,
+  },
+  tasks: [{
+    type: ObjectId,
+    trim: true,
+    required: false
+  }],
+  completed: {
+    type: Boolean,
+    required: [true, "needs completion status"],
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
