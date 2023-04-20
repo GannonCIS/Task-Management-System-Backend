6bcafa6878a5ed87aa133fcf646971143ad4a76b
jared-heidt <jared14heidt@gmail.com>
2023-04-20 13:43:48 -0400

    updated task-controller.js export structure

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -5,18 +5,21 @@ const Task = require('../models/Task');
 * /api/tasks
 * POST Single Album
 */
-exports.insertTask = async(req, res) => {
-
-    const newTask = new Task({
-      name: req.body.name,
-      description: req.body.description,
-      completed: req.body.completed,
-    });
+insertTask = async(req, res) => {
+  const newTask = new Task({
+    name: req.body.name,
+    description: req.body.description,
+    completed: req.body.completed,
+  });
   
-    try {
-      await newTask.save()
-      res.json(newTask);
-    } catch (err) {
-      res.status(400).json( { message: err })
-    }
+  try {
+    await newTask.save()
+    res.json(newTask);
+  } catch (err) {
+    res.status(400).json( { message: err })
   }
+}
+
+module.exports = {
+  insertTask
+}
